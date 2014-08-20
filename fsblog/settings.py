5432acc6dcc5fd6d8ee300788119cf916b909526
franchuk <xcorter@mail.ru>
2014-08-20 22:14:08 +1000

    create new app

--- fsblog/settings.py
+++ fsblog/settings.py
@@ -26,6 +26,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Application definition
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 )
 
 MIDDLEWARE_CLASSES = (
--- fsblog/urls.py
+++ fsblog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'fsblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('main.urls', namespace="main")),
     url(r'^admin/', include(admin.site.urls)),
 )
