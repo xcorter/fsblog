60fb478b4500b79c7cbc7b9c655cf3e6e0071d97
franchuk <xcorter@mail.ru>
2014-10-22 23:39:42 +0700

    добавил статичные файлы

---
+++ fsblog/static/main/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Stepan on 21.08.14.
+ */
+$(document).ready(function(){
+    $('.images').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        slide: 'div',
+        cssEase: 'linear'
+    });
+});
+
+$(document).foundation();
